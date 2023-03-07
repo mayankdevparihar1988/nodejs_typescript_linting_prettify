fc31faf8679df3cd72f40430237ed757767806b0
Mayank Parihar <mayank.parihar@ibsolution.de>
2023-03-07 16:17:33 +0100

    test husky linting

--- src/test.ts
+++ src/test.ts
@@ -2,4 +2,10 @@ import { value } from "./index.js";
 
 const test = "test";
 
+const helloNew = "Hello to test husky liniting";
+
 console.log("Value  : ", value);
+
+console.log("Test husky", helloNew);
+
+console.log("Test the test", test);
