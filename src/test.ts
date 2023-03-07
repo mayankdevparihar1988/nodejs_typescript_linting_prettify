c4990e39fe6ee89179cd8b5d508201ecb7f39fb1
Mayank Parihar <mayank.parihar@ibsolution.de>
2023-03-07 16:59:41 +0100

    adding extended typescript support

--- src/test.ts
+++ src/test.ts
@@ -9,3 +9,13 @@ console.log("Value  : ", value);
 console.log("Test husky", helloNew);
 
 console.log("Test the test", test);
+
+function greetName(name = "anonymous") {
+  if (name) {
+    console.log(name);
+  }
+
+  console.log("HI" + name.toLowerCase());
+}
+
+greetName("Mayank");
